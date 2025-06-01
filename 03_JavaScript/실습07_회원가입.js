ac79ad210d885d95e4ee6ec6d50fdc703103762c
mokoon3930 <rkdtjdah98@gmail.com>
2025-06-01 21:02:59 +0900

    25.06.01 수정

--- 03_JavaScript/실습07_회원가입.js
+++ 03_JavaScript/실습07_회원가입.js
@@ -1,14 +1,14 @@
 const signup = document.querySelector("#signup");
-let check1 = false; // userID
-let check2 = false; // pasword
-let check3 = false; // paswordCheck
+let check1 = false; // userId
+let check2 = false; // password
+let check3 = false; // passwordCheck
 let check4 = false; // userName
 let check5 = false; // email
 let check6 = false; // phone
 
 const userId = document.querySelector("#userId");
-const userIdSpan = document.querySelector("#userId+span");
-const userIdRegExp = /^[a-zA-Z][a-zA-Z0-9]{3,11}$/;
+const userSpan = document.querySelector("#userId+span");
+const userRegExp = /^[a-zA-Z][a-zA-Z0-9]{3,11}$/;
 userId.addEventListener("input", (e) => {
   if (userRegExp.test(e.target.value)) {
     userSpan.innerHTML = "OK!";
@@ -23,10 +23,11 @@ userId.addEventListener("input", (e) => {
     userSpan.style.color = "red";
     check1 = false;
   }
-
-  /*if (check1 && check2 && check3 && check4 && check5 && check6) {
+  /*
+  if (check1 && check2 && check3 && check4 && check5 && check6) {
     signup.disabled = false;
-  }*/
+  }
+    */
 
   signup.disabled = !(check1 && check2 && check3 && check4 && check5 && check6);
 });
@@ -36,7 +37,7 @@ const passwordSpan = document.querySelector("#password+span");
 const passwordCheck = document.querySelector("#passwordCheck");
 const passwordCheckSpan = document.querySelector("#passwordCheck+span");
 
-const passwordRegExp =
+const passRegExp =
   /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,15}$/;
 password.addEventListener("input", (e) => {
   if (passRegExp.test(e.target.value)) {
@@ -53,21 +54,19 @@ password.addEventListener("input", (e) => {
     check2 = false;
   }
 
-  if (passRegExp.test(e.target.value) && passwordCheck.value !== "") {
-    if (passwordCheck.value === e.target.value) {
-      passwordCheckSpan.innerHTML = "OK";
-      passwordCheckSpan.style.color = "green";
-      check2 = true;
-    } else {
-      passwordCheckSpan.innerHTML = "위 비밀번호와 동일하게";
-      passwordCheckSpan.style.color = "red";
-      check2 = false;
-    }
+  if (passwordCheck.value === e.target.value) {
+    passwordCheckSpan.innerHTML = "OK";
+    passwordCheckSpan.style.color = "green";
+    check3 = true;
+  } else {
+    passwordCheckSpan.innerHTML = "위 비밀번호와 동일하게";
+    passwordCheckSpan.style.color = "red";
+    check3 = false;
   }
+
   signup.disabled = !(check1 && check2 && check3 && check4 && check5 && check6);
 });
-const passRegExp =
-  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,15}$/;
+
 passwordCheck.addEventListener("input", (e) => {
   if (passRegExp.test(password.value) && password.value === e.target.value) {
     passwordCheckSpan.innerHTML = "OK!";
@@ -89,68 +88,65 @@ const userName = document.querySelector("#userName");
 const userNameSpan = document.querySelector("#userName+span");
 const userNameRegExp = /^[가-힣]{2,}$/;
 userName.addEventListener("input", (e) => {
-  if (
-    userNameRegExp.test(userName.value) &&
-    userName.value === e.target.value
-  ) {
-    userNameCheckSpan.innerHTML = "OK!";
-    userNameCheckSpan.style.color = "green";
+  if (userNameRegExp.test(e.target.value)) {
+    userNameSpan.innerHTML = "OK!";
+    userNameSpan.style.color = "green";
     check4 = true;
   } else if (e.target.value === "") {
-    userNameSpan.innerHTML = "한글 2글자이상";
+    userNameSpan.innerHTML = "한글 2자 이상";
     userNameSpan.style.color = "black";
     check4 = false;
   } else {
-    userNameSpan.innerHTML = "한글 2글자이상";
+    userNameSpan.innerHTML = "한글 2자 이상";
     userNameSpan.style.color = "red";
     check4 = false;
   }
   signup.disabled = !(check1 && check2 && check3 && check4 && check5 && check6);
 });
 
-const email = document.querySelector("#userName");
-const emailSpan = document.querySelector("#userName+span");
-const emailRegExp = /^[가-힣]{2,}$/;
-userName.addEventListener("input", (e) => {
-  if (emailRegExp.test(email.value) && email.value === e.target.value) {
-    emailCheckSpan.innerHTML = "OK!";
-    emailCheckSpan.style.color = "green";
+const email = document.querySelector("#email");
+const emailSpan = document.querySelector("#email+span");
+const emailRegExp = /^[\w.-]+@[\w.-]+\.[A-Za-z]{2,}$/;
+email.addEventListener("input", (e) => {
+  if (emailRegExp.test(e.target.value)) {
+    emailSpan.innerHTML = "OK!";
+    emailSpan.style.color = "green";
     check5 = true;
   } else if (e.target.value === "") {
-    email.innerHTML = "한글 2글자이상";
-    email.style.color = "black";
+    emailSpan.innerHTML = "이메일 형식";
+    emailSpan.style.color = "black";
     check5 = false;
   } else {
-    emailpan.innerHTML = "한글 2글자이상";
+    emailSpan.innerHTML = "이메일 형식";
     emailSpan.style.color = "red";
     check5 = false;
   }
-
   signup.disabled = !(check1 && check2 && check3 && check4 && check5 && check6);
 });
 
 const phone = document.querySelector("#phone");
 const phoneSpan = document.querySelector("#phone+span");
-const phoneRegExp = /^[가-힣]{2,}$/;
-userName.addEventListener("input", (e) => {
-  if (phoneRegExp.test(phone.value) && phone.value === e.target.value) {
+const phoneRegExp = /^010-\d{4}-\d{4}$/;
+phone.addEventListener("input", (e) => {
+  if (phoneRegExp.test(e.target.value)) {
     phoneSpan.innerHTML = "OK!";
     phoneSpan.style.color = "green";
     check6 = true;
   } else if (e.target.value === "") {
-    phoneSpan.innerHTML = "한글 2글자이상";
+    phoneSpan.innerHTML = "전화번호 형식";
     phoneSpan.style.color = "black";
     check6 = false;
   } else {
-    phoneSpan.innerHTML = "한글 2글자이상";
+    phoneSpan.innerHTML = "전화번호 형식";
     phoneSpan.style.color = "red";
     check6 = false;
   }
   signup.disabled = !(check1 && check2 && check3 && check4 && check5 && check6);
 });
+
 const cancel = document.querySelector("#cancel");
-cancel.addEventListener("clik", () => {
-  userSpan.innerHTML = "영문잘로 시작하고 영문자롸 숫자 조합으로 4~12자 이내";
+cancel.addEventListener("click", () => {
+  userSpan.innerHTML = "영문자로 시작하고 영문자와 숫자 조합으로 4~12자 이내";
   userSpan.style.color = "black";
   passwordSpan.innerHTML = "영문자, 숫자, 특수문자 조합으로 8~15자 이내";
   passwordSpan.style.color = "black";
--- 03_JavaScript/실습08_로그인.js
+++ 03_JavaScript/실습08_로그인.js
@@ -1,39 +1,30 @@
-//코드 중복! 함수로 기능으로
-const show = (info) => {
+// 코드 중복! 함수로 기능으로
+const show = (name) => {
   document.body.innerHTML = `
-  <h1>${info}님 환영합니다</h1>
-  <div>
-  <button type="button" id="logout">로그아웃</button>
-  </div>`;
+    <h1>${name}님 환영합니다!</h1>
+    <button type="button" id="logout">로그아웃</button>
+  `;
+  const logout = document.querySelector("#logout");
+  logout.addEventListener("click", () => {
+    localStorage.removeItem("name");
+    location.reload();
+  });
 };
 
-//로그인이 되어있는지 안니지 상태확인
+// 로그인 되어 있는 건지 아닌지 상태 확인
 const info = localStorage.getItem("name");
 if (info) {
-  document.body.innerHTML = `
-  <h1>${info}님 환영합니다</h1>
-  <div>
-  <button type="button" id="logout">로그아웃</button>
-  </div>`;
+  show(info);
 }
-const logout = document.querySelector("#logout");
-logout.addEventListener("click", () => {
-  localStorage.removeItem("name");
-  location.reload();
-});
 
-login.addEventListener("chick", () => {
-  //아이디 입력하고 비밀번호 입력받은 값들 로그인 처리
-  //로그인 성공하고 이름을 저장
+login.addEventListener("click", () => {
+  // 이름 입력하고 비밀번호 입력받은 값들 로그인 처리
+  // 로그인 성공하고 이름을 저장
   if (username.value === "" && password.value === "") {
-    alert("이름과 비밀번호를 입력해주세요");
+    alert("이름과 비밀번호를 입력해주세요!");
     return;
   }
   localStorage.setItem("name", username.value);
-  // ~님 환영합니다! 로그아웃 바튼
-  document.body.innerHTML = `
-  <h1>${username.value}님 환영합니다</h1>
-  <div>
-  <button type="button" id="logout">로그아웃</button>
-  </div>`;
+  // ~~님 환영합니다! 로그아웃 버튼
+  show(username.value);
 });
