23f1fc3b68b2fec53e0b9fa7e0569f5ad549dc14
mokoon3930 <rkdtjdah98@gmail.com>
2025-06-03 22:44:56 +0900

    25.06.03 수정

--- 03_JavaScript/실습09_코인정보.js
+++ 03_JavaScript/실습09_코인정보.js
@@ -1,33 +1,34 @@
 /*
 https://api.upbit.com/v1/market/all
-{
+ {
     "market": "KRW-BTC",
     "korean_name": "비트코인",
     "english_name": "Bitcoin"
   },
 */
-//async function dataFetch () {}
-//const dataFetch = async function () {}
-
+// async function dataFetch() {}
+// const dataFetch = async function() {}
 const dataFetch = async () => {
   const response = await fetch("https://api.upbit.com/v1/market/all");
   const data = await response.json();
   console.log(data);
-  /*for (let i = 0; i < data.lenght; i++) {
+  /*
+  for (let i = 0; i < data.length; i++) {
     console.log(data[i]);
   }*/
   /*
   data.forEach((item) => {
-    //startsWith : 시작하는 단어, includes : 포함여부
+    // startsWith : 시작하는 단어 여부, includes : 포함 여부
     if (item.market.includes("KRW")) {
       market.innerHTML += `<h2>${item.korean_name}(${item.market})</h2>`;
     }
-    console.log(item.market.includes("KRW"));
+    console.log(item.market, item.market.includes("KRW"));
     console.log(item.korean_name);
-  });*/
-
+  });
+  */
   // forEach + 조건문 -> filter
   const krwMarkets = data.filter((item) => item.market.includes("KRW"));
+
   /*
   let markets = "";
   krwMarkets.forEach((item) => {
@@ -35,7 +36,7 @@ const dataFetch = async () => {
   });
   console.log(markets.slice(0, -1));
   */
-  //forEach + 내가 원하는 형태로 가공하고자 할때 -> map
+  // forEach + 내가 원하는 형태로 가공하고자 할때 -> map
   const markets = krwMarkets.map((item) => item.market).join(",");
 
   const tickerResponse = await fetch(
@@ -45,13 +46,19 @@ const dataFetch = async () => {
   console.log(tickerData);
 
   krwMarkets.forEach((item) => {
-    tickerData.find((ticker) => ticker.market === item.market);
-    market.innerHTML += `<h2>
-        ${item.korean_name}(${item.market})</h2>
-      <p>현재가 : ${ticker.trade_price.toLocaleString()}원</p>
-      <p>24시간 거래량 :${ticker.acc_trade_volume_24h.toFoixed(2)}
-      <p>전일 대비 :${ticker.change === 'RISE' ? "상승" : ticker.change === 'FALL' ? "하락" : "보합"} (${(ticker.change_rate * 100.toFixed(2))}%)</p>
-      `;
+    const ticker = tickerData.find((ticker) => ticker.market === item.market);
+    market.innerHTML += `<h2>${item.korean_name}(${item.market})</h2>
+        <p>현재가 : ${ticker.trade_price.toLocaleString()}원</p>
+        <p>24시간 거래량 : ${ticker.acc_trade_volume_24h.toFixed(2)}
+        <p>전일 대비 : ${
+          ticker.change === "RISE"
+            ? "상승"
+            : ticker.change === "FALL"
+            ? "하락"
+            : "보합"
+        } 
+        (${(ticker.change_rate * 100).toFixed(2)}%)</p>
+    `;
   });
 };
 dataFetch();
