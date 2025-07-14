c93257048b742b1c32cc35123fe1853873f58e9f
mokoon3930 <rkdtjdah98@gmail.com>
2025-07-14 17:35:36 +0900

    25.07.14

---
+++ 09_Spring/01_MyBatis/src/main/java/com/kh/mybatis/mapper/MemberMapper.java
@@ -0,0 +1,15 @@
+package com.kh.mybatis.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.kh.mybatis.model.vo.Member;
+
+@Mapper
+public interface MemberMapper {
+	void register(Member vo);
+	Member login(Member vo);
+	List<Member> allMember();
+	void update(Member vo);
+}
