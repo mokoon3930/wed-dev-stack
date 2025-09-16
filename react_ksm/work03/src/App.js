a8640c9bf30906a0df6bfbda836e541e300844be
mokoon3930 <rkdtjdah98@gmail.com>
2025-09-16 14:10:02 +0900

    ..

---
+++ react_ksm/work03/src/App.js
@@ -0,0 +1,35 @@
+import logo from "./logo.svg";
+
+import { useState } from "react";
+
+function App() {
+  let [num, setNum] = useState(0);
+
+  function down() {
+    setNum(num - 1);
+  } // 함수를 따로 빼 줄수 있음
+
+  return (
+    <div className="App">
+      <button onClick={down}>-</button>
+      <button
+        onClick={(e) => {
+          setNum(0);
+        }}
+      >
+        0
+      </button>
+      <button
+        onClick={(e) => {
+          setNum(num + 1);
+        }}
+      >
+        +
+      </button>
+      <input type="text">{num}</input>
+      <samp>{num}</samp>
+    </div>
+  );
+}
+
+export default App;
