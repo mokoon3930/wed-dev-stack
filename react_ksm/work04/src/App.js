bc12c3f9f89c1221051e99252a0b98bf0a5f86f4
mokoon3930 <rkdtjdah98@gmail.com>
2025-09-16 16:41:14 +0900

    25.09.16 react 과제

--- react_ksm/work04/src/App.js
+++ react_ksm/work04/src/App.js
@@ -3,7 +3,6 @@ import logo from "./logo.svg";
 
 function App() {
   let [greet, setGreet] = useState("");
-  let [msg, setMsg] = useState("");
 
   function nameChange(e) {
     e.preventDefault();
@@ -13,18 +12,6 @@ function App() {
     setGreet("안녕하세요" + name + "님, 오늘의 기분은?");
   }
 
-  function moodChange(e) {
-    let mood = e.target.value;
-    if (mood === "행복") {
-      setMsg("행복 하군요");
-    } else if (mood === "슬픔") {
-      setMsg("ㅠㅠ");
-    } else if (mood === "화남") {
-      setMsg("화가 나내요");
-    } else {
-      setMsg();
-    }
-  }
   return (
     <div className="App">
       <form onSubmit={nameChange}>
@@ -39,19 +26,40 @@ function App() {
 
       <p>{greet}</p>
 
+      <MyMood></MyMood>
       {/*기분을 선택하는 select 태그*/}
-      <div>
-        <select onChange={moodChange}>
-          <option>기분을 선택 하세요</option>
-          <option value="행복">행복</option>
-          <option value="슬픔">슬픔</option>
-          <option value="화남">화남</option>
-        </select>
-
-        <p>{msg}</p>
-      </div>
 
       <br />
+    </div>
+  );
+}
+
+function MyMood(props) {
+  let [msg, setMsg] = useState("");
+
+  function moodChange(e) {
+    let mood = e.target.value;
+    if (mood === "행복") {
+      setMsg("행복 하군요");
+    } else if (mood === "슬픔") {
+      setMsg("ㅠㅠ");
+    } else if (mood === "화남") {
+      setMsg("화가 나내요");
+    } else {
+      setMsg();
+    }
+  }
+
+  return (
+    <div>
+      <select onChange={moodChange}>
+        <option>기분을 선택 하세요</option>
+        <option value="행복">행복</option>
+        <option value="슬픔">슬픔</option>
+        <option value="화남">화남</option>
+      </select>
+
+      <p>{msg}</p>
     </div>
   );
 }
